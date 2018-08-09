fe924dc51756117283222f552485e328775edbf8
Giovanni dos Santos <giovanni.santos.1811@gmail.com>
2018-08-09 20:43:42 -0300

    Implementado o design e o funcionamento do botao de menu

---
+++ Assets/Scripts/GameControler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControler : MonoBehaviour {
+
+    public GameObject menuOpcoes;
+    public bool status { get; private set; }
+
+	// Use this for initialization
+	void Start () {
+        status = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ativaMenu()
+    {
+        status = !status;
+        menuOpcoes.SetActive(status);
+    }
+}
